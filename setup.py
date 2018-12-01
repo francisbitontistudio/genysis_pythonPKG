05fe8885b9f098f6f26d438d51246094f521cd77
francisbitontistudio <francis@lexset.ai>
2018-12-01 17:11:57 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
-from setuptools import setup
 
-setup(
-    name="genysis",
-    version="0.0.1",
-    scripts=['genysis.py']
+setup(name='genysis',
+      description='An OpenSSL-based file encryption
+                   and decryption utility',
+      long_description=long_description,
+      version='0.0.1',
+      url='https://github.com/francisbitontistudio/genysis_pythonPKG.git',
+      author='F. Bitonti',
+      author_email='Francis@studiobitonti.com',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: CAD',
+          'Programming Language :: Python :: 2'
+      ],
+      packages=['genysis'],
+      install_requires=[
+          'json>=18.2',
+          'ast>=32.2'
+      ],
+      entry_points={
+          'console_scripts': [
+              'encrypt=crytto.main:run'
+          ]
+      }
 )
